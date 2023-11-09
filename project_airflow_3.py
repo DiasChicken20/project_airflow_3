917a078f11cd370c761d4e237349ee67f8f04c47
Dias@~__* <120366637+DiasChicken20@users.noreply.github.com>
2023-11-09 13:27:19 +0800

    Update project_airflow_3.py

--- project_airflow_3.py
+++ project_airflow_3.py
@@ -7,38 +7,119 @@ Kelompok: 4
 
 
 
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.types import BigInteger, String, JSON, DateTime, Boolean, Float, Integer
 import requests
 import json
 import pandas as pd
-from sqlalchemy import create_engine
 
-url = "https://api.openaq.org/v2/locations/5241"
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
 
-headers = {"X-API-Key": "95ed1888290e5829910cdc3736948562bc942f0d1ac292c8b3b3cffc48aa72c1"}
+default_args = {
+    "owner": "kelp_4",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5)
+}
+    # --Extract--
+def extract_url(**content):
+    url = 'https://api.openaq.org/v2/locations/5241'
+    headers = {'X-API-Key': '95ed1888290e5829910cdc3736948562bc942f0d1ac292c8b3b3cffc48aa72c1'}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    print(data)
+    for row in data['results']:
+        print(row)
+    
 
-response = requests.get(url, headers=headers)
+# # Load to local file
+    with open('dataset/file.json', 'w') as f:
+        json.dump(row, f)
 
-data = response.json()
+    return content['ti'].xcom_push(key='hole_data', value=f)
+# read json file
+pd.set_option('display.max_columns', None)
+df = pd.read_json('dataset/file.json', lines= True)
+    
 
-with open("data3.json", "w") as file:
-    json.dump(data,file)
+df = pd.read_json('dataset/file.json', lines= True)
+
+# --Transform--
+def transform_data(**content):
+    df['entity'] = df['entity'].astype('string')
+    df['sources'] = df['sources'].astype('string')
+    df['sensorType'] = df['sensorType'].astype('string')
+    df['isMobile'] = df['isMobile'].astype('boolean')
+    df['isAnalysis'] = df['isAnalysis'].astype('boolean')
+    df['measurements'] = df['measurements'].astype('string')
+    print(df.dtypes)
+    return content['ti'].xcom_push(key='transform_data', value=df)
+
+# --Load--
+def load_data():
+    df_schema = {
+    'id': BigInteger,
+    'city': String(100),
+    'name': String(100),
+    'entity': String(100),
+    'country': String(100),
+    'sources': String(100),
+    'isMobile': Boolean,
+    'isAnalysis': Boolean,
+    'parameters': JSON,
+    'sensorType': String(100),
+    'coordinates': JSON,
+    'lastUpdated': String(100),
+    'measurements': String(100),
+    'bounds': JSON,
+    'manufacturers': JSON,
+}
+    user = 'djamier'
+    password = 'djamier'
+    host = 'localhost'
+    database = 'postgres'
+    port = '5435' 
+
+    conn_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+    engine = create_engine(conn_string)
+
+    return df.to_sql(name= 'data_json', con=engine, schema= 'public', if_exists='replace', index= None, dtype=df_schema)
+
+kelp_4_dag = DAG(
+    dag_id = 'etl_klp_4_dag',
+    default_args = default_args,
+    start_date = datetime(2023, 11, 7),
+    catchup = False,
+    schedule = "0 4 * * *"
+)
 
 
-with open("data3.json", "r") as json_file:
-    data = json.load(json_file)
+extract_data_task = PythonOperator(
+    task_id = 'extract_data_task',
+    python_callable = extract_url,
+    provide_context = True,
+    dag = kelp_4_dag
+) 
 
-df = pd.DataFrame(data)
+transform_data_task = PythonOperator(
+    task_id = 'transform_data_task',
+    python_callable = transform_data,
+    provide_context = True,
+    dag = kelp_4_dag
+)
 
-database_url = 'sqllite:///new_database.db'
+load_data_task = PythonOperator(
+    task_id = 'load_data_task',
+    python_callable = load_data,
+    provide_context = True,
+    dag = kelp_4_dag
+)
 
-engine = create_engine(database_url)
-
-table_name =  'python_script'
-
-df.to_sql(table_name, con=engine, if_exist='replace', index=False)
-
-engine.dispose()
-
+extract_data_task >> transform_data_task >> load_data_task
 
 
               
